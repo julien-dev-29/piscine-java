fe1519dae5011b2503973e088c11b66e89b5ad56
jurol <rollandjulien718@gmail.com>
2026-05-25 17:58:57 +0200

    add IsEven exercise solution

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,33 +1,11 @@
-import Adventure.DeadCharacterException;
-import Adventure.Sorcerer;
-import Adventure.Templar;
-import Adventure.Weapon;
+import IsEven.IsEven;
 
 import java.io.IOException;
 
 public class ExerciseRunner {
     public static void main(String[] args) throws IOException {
-        Weapon excalibur = new Weapon("Excalibur", 7);
-        Weapon baton = new Weapon("Baton", 3);
-        Templar arthur = new Templar("Arthur", 30, 5, 3, excalibur);
-        Sorcerer merlin = new Sorcerer("Merlin", 28, 2, baton);
-
-        try {
-            arthur.takeDamage(50);
-        } catch (DeadCharacterException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            arthur.takeDamage(2);
-        } catch (DeadCharacterException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            arthur.attack(merlin);
-        } catch (DeadCharacterException e) {
-            System.out.println(e.getMessage());
-        }
-
+        System.out.println(IsEven.isEven(2));
+        System.out.println(IsEven.isEven(26));
+        System.out.println(IsEven.isEven(57));
     }
 }
