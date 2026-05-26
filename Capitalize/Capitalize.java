58fc5aed53aa7cc87f2796a3107ce5b7b58a8a65
jurol <rollandjulien718@gmail.com>
2026-05-26 10:22:57 +0200

    add Capitalize exercise solution

---
+++ Capitalize/Capitalize.java
@@ -0,0 +1,32 @@
+package Capitalize;
+
+import java.io.*;
+
+public class Capitalize {
+
+    public static void capitalize(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+        String line;
+        boolean firstLine = true;
+        while ((line = reader.readLine()) != null) {
+            if (!firstLine) {
+                writer.newLine();
+            }
+            String[] words = line.split(" ");
+            for (int i = 0; i < words.length; i++) {
+
+                if (!words[i].isEmpty()) {
+                    words[i] =
+                            words[i].substring(0, 1).toUpperCase()
+                                    + words[i].substring(1).toLowerCase();
+                }
+            }
+
+            writer.write(String.join(" ", words));
+            firstLine = false;
+        }
+        reader.close();
+        writer.close();
+    }
+}
--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,10 +1,14 @@
-import Palindrome.Palindrome;
+import Capitalize.Capitalize;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ExerciseRunner {
     public static void main(String[] args) throws IOException {
-        System.out.println(Palindrome.isPalindrome("ressasser"));
-        System.out.println(Palindrome.isPalindrome("Bonjour"));
+        Capitalize.capitalize(new String[]{"input.txt", "output.txt"});
+        String expectedResult = new String(Files.readAllBytes(Paths.get("result.txt")));
+        String userOutput = new String(Files.readAllBytes(Paths.get("output.txt")));
+        System.out.println(expectedResult.equals(userOutput));
     }
 }
