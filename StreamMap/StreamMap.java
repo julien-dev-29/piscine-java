0c97096d6d74c2fb3c34da4b420805710a60845a
jurol <rollandjulien718@gmail.com>
2026-06-10 20:07:38 +0200

    add StreamMap exercice solution

---
+++ StreamMap/StreamMap.java
@@ -0,0 +1,22 @@
+package StreamMap;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamMap {
+    public static Integer sumOfStringLength(Stream<String> s) {
+        return s.mapToInt(String::length).sum();
+    }
+
+    public static List<String> upperCaseAllString(Stream<String> s) {
+        return s.map(String::toUpperCase).toList();
+    }
+
+    public static Set<Integer> uniqIntValuesGreaterThan42(Stream<Double> s) {
+        return s
+                .filter(aDouble -> aDouble > 42)
+                .map(Double::intValue).collect(Collectors.toSet());
+    }
+}
